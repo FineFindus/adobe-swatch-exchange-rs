80afe0ae6342e49d4ceb0042ced196445f2afc9b
FineFindus <FineFindus@proton.me>
2023-09-22 22:24:42 +0200

    feat: add buffer

---
+++ src/buffer.rs
@@ -0,0 +1,48 @@
+/// Represents an infinite buffer designed to hold individual bytes (`u8`).
+///
+/// It provides methods for easily writing other data types
+/// as bytes.
+#[derive(Debug, Clone)]
+pub(crate) struct Buffer {
+    data: Vec<u8>,
+}
+
+impl Buffer {
+    /// Create a new Buffer with the specified capacity.
+    pub(crate) fn with_capacity(capacity: usize) -> Self {
+        Self {
+            data: Vec::with_capacity(capacity),
+        }
+    }
+
+    /// Write the slice to self.
+    pub fn write_slice(&mut self, src: &[u8]) {
+        self.data.extend_from_slice(src);
+    }
+
+    /// Write u32 in big-endian order to self.
+    pub fn write_u32(&mut self, n: u32) {
+        self.write_slice(&n.to_be_bytes());
+    }
+
+    /// Write f32 in big-endian order to self.
+    pub fn write_f32(&mut self, n: f32) {
+        self.write_slice(&n.to_be_bytes());
+    }
+
+    /// Write u16 in big-endian order to self.
+    pub fn write_u16(&mut self, n: u16) {
+        self.write_slice(&n.to_be_bytes());
+    }
+
+    /// Write a null terminated UTF16 String to self.
+    pub fn write_null_terminated_utf_16_str(&mut self, src: &str) {
+        src.encode_utf16().for_each(|byte| self.write_u16(byte));
+        self.write_u16(0);
+    }
+
+    /// Returns the written buffer as a [`Vec<u8>`] of bytes.
+    pub fn to_vec(self) -> Vec<u8> {
+        self.data
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+const FILE_SIGNATURE: &[u8; 4] = b"ASEF";
+const VERSION: u32 = 0x00010000;
+
+#[derive(Debug, Clone)]
+enum BlockType {
+    GroupStart,
+    GroupEnd,
+    ColorEntry,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone)]
+enum ColorType {
+    Global,
+    Spot,
+    Normal,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone)]
+enum ColorModel {
+    CMYK,
+    RGB,
+    LAB,
+    Gray,
+}
+
+#[derive(Debug, Clone)]
+enum ColorValue {
+    CMYK(f32, f32, f32, f32),
+    RGB(f32, f32, f32),
+    LAB(f32, f32, f32),
+    Gray(f32),
+}
+
+#[derive(Debug, Clone)]
+struct Block<'a> {
+    pub block_type: BlockType,
+    pub name: &'a str,
+    pub color_type: ColorType,
+    pub color: ColorType,
+}
+
+pub fn create_ase() -> Vec<u8> {
+    let buf = Vec::new();
+    buf.append(FILE_SIGNATURE);
+    vec![]
 }

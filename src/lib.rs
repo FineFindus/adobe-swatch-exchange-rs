b15f42f5029586f741e8105159e92447e77e5740
FineFindus <FineFindus@proton.me>
2023-09-23 13:21:09 +0200

    feat: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,85 @@ pub fn create_ase(groups: Vec<Group>, colors: Vec<ColorBlock>) -> Vec<u8> {
 
     buf.to_vec()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_writes_empty_args() {
+        assert_eq!(
+            create_ase(vec![], vec![]),
+            vec![65, 83, 69, 70, 0, 1, 0, 0, 0, 0, 0, 0]
+        )
+    }
+
+    #[test]
+    fn it_writes_single_color() {
+        let block = ColorBlock::new("name", ColorValue::Gray(0.5), ColorType::Normal);
+        assert_eq!(
+            create_ase(vec![], vec![block]),
+            vec![
+                65, 83, 69, 70, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 18, 0, 5, 0, 110, 0,
+                97, 0, 109, 0, 101, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2
+            ]
+        )
+    }
+
+    #[test]
+    fn it_writes_group_color() {
+        let group = Group::new(
+            "group name",
+            vec![
+                ColorBlock::new("light grey", ColorValue::Gray(0.5), ColorType::Normal),
+                ColorBlock::new(
+                    "dark red",
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let block = ColorBlock::new("name", ColorValue::Gray(0.5), ColorType::Normal);
+        assert_eq!(
+            create_ase(vec![group], vec![block]),
+            vec![
+                65, 83, 69, 70, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 192, 1, 0, 0, 0, 72, 0, 11, 0, 103,
+                0, 114, 0, 111, 0, 117, 0, 112, 0, 32, 0, 110, 0, 97, 0, 109, 0, 101, 0, 0, 0, 0,
+                0, 1, 0, 0, 0, 24, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0, 32, 0, 103, 0,
+                114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0,
+                0, 30, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0, 100, 0, 0,
+                82, 71, 66, 32, 63, 0, 0, 0, 62, 153, 153, 154, 61, 204, 204, 205, 0, 2, 0, 0, 192,
+                2, 0, 0, 0, 1, 0, 0, 0, 18, 0, 5, 0, 110, 0, 97, 0, 109, 0, 101, 0, 0, 71, 114, 97,
+                121, 63, 0, 0, 0, 0, 2,
+            ]
+        )
+    }
+    #[test]
+    fn it_writes_group_and_single_color() {
+        let group = Group::new(
+            "group name",
+            vec![
+                ColorBlock::new("light grey", ColorValue::Gray(0.5), ColorType::Normal),
+                ColorBlock::new(
+                    "dark red",
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let block = ColorBlock::new("name", ColorValue::Gray(0.5), ColorType::Normal);
+        assert_eq!(
+            create_ase(vec![group], vec![block]),
+            vec![
+                65, 83, 69, 70, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 192, 1, 0, 0, 0, 72, 0, 11, 0, 103,
+                0, 114, 0, 111, 0, 117, 0, 112, 0, 32, 0, 110, 0, 97, 0, 109, 0, 101, 0, 0, 0, 0,
+                0, 1, 0, 0, 0, 24, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0, 32, 0, 103, 0,
+                114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0,
+                0, 30, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0, 100, 0, 0,
+                82, 71, 66, 32, 63, 0, 0, 0, 62, 153, 153, 154, 61, 204, 204, 205, 0, 2, 0, 0, 192,
+                2, 0, 0, 0, 1, 0, 0, 0, 18, 0, 5, 0, 110, 0, 97, 0, 109, 0, 101, 0, 0, 71, 114, 97,
+                121, 63, 0, 0, 0, 0, 2
+            ]
+        )
+    }
+}

7ed06c4ae2b53e0ab712d5ff01efafc1684fc3aa
FineFindus <FineFindus@proton.me>
2023-09-23 13:09:33 +0200

    feat(types/Group): add tests

--- src/types/group.rs
+++ src/types/group.rs
@@ -47,3 +47,54 @@ impl<'a> Group<'a> {
                 .sum::<u32>()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{ColorType, ColorValue};
+
+    use super::*;
+
+    #[test]
+    fn it_calculates_length_correctly() {
+        let group = Group::new(
+            "group na,e",
+            vec![
+                ColorBlock::new("light grey", ColorValue::Gray(0.5), ColorType::Normal),
+                ColorBlock::new(
+                    "dark red",
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        assert_eq!(group.calculate_length(), 72);
+    }
+
+    #[test]
+    fn it_writes_bytes_correctly() {
+        let group = Group::new(
+            "group name",
+            vec![
+                ColorBlock::new("light grey", ColorValue::Gray(0.5), ColorType::Normal),
+                ColorBlock::new(
+                    "dark red",
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let mut buf = Buffer::with_capacity(72);
+        group.write(&mut buf);
+        assert_eq!(
+            buf.to_vec(),
+            vec![
+                0, 0, 192, 1, 0, 0, 0, 72, 0, 11, 0, 103, 0, 114, 0, 111, 0, 117, 0, 112, 0, 32, 0,
+                110, 0, 97, 0, 109, 0, 101, 0, 0, 0, 0, 0, 1, 0, 0, 0, 24, 0, 11, 0, 108, 0, 105,
+                0, 103, 0, 104, 0, 116, 0, 32, 0, 103, 0, 114, 0, 101, 0, 121, 0, 0, 71, 114, 97,
+                121, 63, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 30, 0, 9, 0, 100, 0, 97, 0, 114, 0,
+                107, 0, 32, 0, 114, 0, 101, 0, 100, 0, 0, 82, 71, 66, 32, 63, 0, 0, 0, 62, 153,
+                153, 154, 61, 204, 204, 205, 0, 2, 0, 0, 192, 2
+            ]
+        );
+    }
+}

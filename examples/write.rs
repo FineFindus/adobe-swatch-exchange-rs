9ce30f6f9c1eacade58110526ea4f5f171afd14f
FineFindus <FineFindus@proton.me>
2023-10-03 12:17:39 +0200

    feat(example): write ase to local file

--- examples/write.rs
+++ examples/write.rs
@@ -1,38 +1,37 @@
 #![allow(unused)]
 use adobe_swatch_exchange::{ColorBlock, ColorType, ColorValue, Group};
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let group = Group::new(
         "Aurora".to_owned(),
         vec![
             ColorBlock::new(
                 "#BF616A".to_owned(),
-                ColorValue::Rgb(0.7490196078431373, 0.3803921568627451, 0.41568627450980394),
+                ColorValue::Rgb(0.749_019_6, 0.380_392_16, 0.415_686_28),
                 ColorType::Normal,
             ),
             ColorBlock::new(
                 "#D08770".to_owned(),
-                ColorValue::Rgb(0.8156862745098039, 0.5294117647058824, 0.4392156862745098),
+                ColorValue::Rgb(0.815_686_3, 0.529_411_8, 0.439_215_7),
                 ColorType::Normal,
             ),
             ColorBlock::new(
                 "#EBCB8B".to_owned(),
-                ColorValue::Rgb(0.9215686274509803, 0.796078431372549, 0.5450980392156862),
+                ColorValue::Rgb(0.921_568_63, 0.796_078_44, 0.545_098_07),
                 ColorType::Normal,
             ),
             ColorBlock::new(
                 "#A3BE8C".to_owned(),
-                ColorValue::Rgb(0.6392156862745098, 0.7450980392156863, 0.5490196078431373),
+                ColorValue::Rgb(0.639_215_7, 0.745_098_05, 0.549_019_63),
                 ColorType::Normal,
             ),
             ColorBlock::new(
                 "#B48EAD".to_owned(),
-                ColorValue::Rgb(0.7058823529411765, 0.5568627450980392, 0.6784313725490196),
+                ColorValue::Rgb(0.705_882_4, 0.556_862_8, 0.678_431_4),
                 ColorType::Normal,
             ),
         ],
     );
     let ase = adobe_swatch_exchange::create_ase(vec![group], vec![]);
-    // do something with the file, e.g. write it
-    // std::fs::write("aurora.ase", ase)
+    std::fs::write("examples/aurora.ase", ase)
 }

07f6a2923a78b8b81567729c84a4cb4df0d9046c
FineFindus <FineFindus@proton.me>
2023-12-24 09:40:36 +0100

    feat(group): safely read bytes
    Use .get() to safely read bytes, since the slice may end prematurely, causing a out-of-bounds panic.

--- src/types/group.rs
+++ src/types/group.rs
@@ -71,9 +71,16 @@ impl Group {
     /// This function will return an error if either the name cannot be constructed, or
     /// if it cannot be correctly parsed. In either case an [`ASEError::Invalid`] is returned.
     pub(crate) fn parse(bytes: &[u8]) -> Result<Self, ASEError> {
-        let name_length = u16::from_be_bytes(bytes[0..2].try_into()?);
+        let name_length = u16::from_be_bytes(
+            bytes
+                .get(0..2)
+                .ok_or(ASEError::InputDataParseError)?
+                .try_into()?,
+        );
         //read name bytes, but stop before not byte
-        let name_bytes: Vec<u16> = bytes[2..(name_length as usize * 2)]
+        let name_bytes: Vec<u16> = bytes
+            .get(2..(name_length as usize * 2))
+            .ok_or(ASEError::InputDataParseError)?
             .chunks_exact(2)
             .map(|bytes| u16::from_be_bytes(bytes.try_into().unwrap()))
             .collect();
@@ -87,7 +94,10 @@ impl Group {
             }
 
             let block_type = BlockType::try_from(u16::from_be_bytes(
-                bytes[pointer..(pointer + 2)].try_into()?,
+                bytes
+                    .get(pointer..(pointer + 2))
+                    .ok_or(ASEError::InputDataParseError)?
+                    .try_into()?,
             ))?;
 
             if block_type != BlockType::ColorEntry {
@@ -95,11 +105,17 @@ impl Group {
             }
             pointer += 2;
 
-            let block_length =
-                u32::from_be_bytes(bytes[pointer..(pointer + 4)].try_into()?) as usize;
+            let block_length = u32::from_be_bytes(
+                bytes
+                    .get(pointer..(pointer + 4))
+                    .ok_or(ASEError::InputDataParseError)?
+                    .try_into()?,
+            ) as usize;
             pointer += 4;
 
-            let Ok(block) = ColorBlock::parse(&bytes[pointer..]) else {
+            let Ok(block) =
+                ColorBlock::parse(&bytes.get(pointer..).ok_or(ASEError::InputDataParseError)?)
+            else {
                 break;
             };
             pointer += block_length;
@@ -226,5 +242,129 @@ mod tests {
             ])
             .unwrap()
         );
+    }
+
+    #[test]
+    fn it_returns_error_on_empty_input() {
+        let parser_result = Group::parse(&[]);
+        assert!(
+            matches!(parser_result.err(), Some(ASEError::InputDataParseError)),
+            "Only ASEError::InputDataParseError should be returned"
+        );
+    }
+
+    #[test]
+    fn it_returns_error_on_length_larger_than_input() {
+        // try to parse more name bytes than available
+        let parser_result = Group::parse(&[12, 34]);
+        assert!(
+            matches!(parser_result.err(), Some(ASEError::InputDataParseError)),
+            "Only ASEError::InputDataParseError should be returned"
+        );
+    }
+
+    #[test]
+    fn it_returns_error_on_invalid_utf_16() {
+        // `[0xDC, 0x00]` is invalid utf16
+        let parser_result = Group::parse(&[0, 5, 0xDC, 0x00, 0, 97, 0, 109, 0, 101]);
+        assert!(
+            matches!(parser_result.err(), Some(ASEError::UTF16Error)),
+            "Only ASEError::UTF16Error should be returned"
+        );
+    }
+
+    #[test]
+    fn it_returns_error_on_invalid_block_type() {
+        // 0, 255 is not a valid block type
+        let parser_result = Group::parse(&[0, 1, 0, 0, 0, 255]);
+        assert!(
+            matches!(parser_result.err(), Some(ASEError::BlockTypeError)),
+            "Only ASEError::BlockTypeError should be returned"
+        );
+    }
+
+    #[test]
+    fn it_returns_on_invalid_block_type() {
+        let group = Group::new(
+            "".to_owned(),
+            vec![ColorBlock::new(
+                "light grey".to_owned(),
+                ColorValue::Gray(0.5),
+                ColorType::Normal,
+            )],
+        );
+        assert_eq!(
+            group,
+            Group::parse(&[
+                0, 1, 0, 0, 0, 1, 0, 0, 0, 34, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0,
+                32, 0, 103, 0, 114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2,
+                // second block, start with 0xc001 (GroupStart) instead of 0x0001 (ColorEntry), so
+                // that parser stops early
+                0xc0, 1, 0, 0, 0, 38, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0,
+                100, 0, 0, 82, 71, 66, 32, 63, 0, 0, 0, 62, 153, 153, 154, 61, 204, 204, 205, 0, 2,
+                192, 2
+            ])
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn it_returns_on_invalid_block() {
+        let group = Group::new(
+            "".to_owned(),
+            vec![ColorBlock::new(
+                "light grey".to_owned(),
+                ColorValue::Gray(0.5),
+                ColorType::Normal,
+            )],
+        );
+        assert_eq!(
+            group,
+            Group::parse(&[
+                0, 1, 0, 0, 0, 1, 0, 0, 0, 34, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0,
+                32, 0, 103, 0, 114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2,
+                // second block, invalid, since half of it is missing
+                0, 1, 0, 0, 0, 38, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0,
+                100, 0, 0, 82, 71, 66, 32, 63, 0, 0
+            ])
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn it_returns_error_on_invalid_block_length() {
+        let parser_result = Group::parse(&[
+            //has block length of `34`, replacing it with invalid length of 13
+            0, 1, 0, 0, 0, 1, 0, 0, 0, 13, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0, 32, 0,
+            103, 0, 114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0,
+            38, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0, 100, 0, 0, 82, 71,
+            66, 32, 63, 0, 0, 0, 62, 153, 153, 154, 61, 204, 204, 205, 0, 2, 192, 2,
+        ]);
+        assert!(parser_result.is_err());
+        assert!(
+            matches!(parser_result.err(), Some(ASEError::BlockTypeError)),
+            "Only ASEError::InputDataParseError should be returned"
+        );
+    }
+
+    #[test]
+    fn it_returns_block_on_invalid_block_length() {
+        let group = Group::new(
+            "".to_owned(),
+            vec![ColorBlock::new(
+                "light grey".to_owned(),
+                ColorValue::Gray(0.5),
+                ColorType::Normal,
+            )],
+        );
+        let parser_result = Group::parse(&[
+            //has block length of `34`, replacing it with invalid length of 130
+            0, 1, 0, 0, 0, 1, 0, 0, 0, 130, 0, 11, 0, 108, 0, 105, 0, 103, 0, 104, 0, 116, 0, 32, 0,
+            103, 0, 114, 0, 101, 0, 121, 0, 0, 71, 114, 97, 121, 63, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0,
+            38, 0, 9, 0, 100, 0, 97, 0, 114, 0, 107, 0, 32, 0, 114, 0, 101, 0, 100, 0, 0, 82, 71,
+            66, 32, 63, 0, 0, 0, 62, 153, 153, 154, 61, 204, 204, 205, 0, 2, 192, 2,
+        ]);
+        assert!(parser_result.is_ok());
+        assert_eq!(group, parser_result.unwrap());
     }
 }
